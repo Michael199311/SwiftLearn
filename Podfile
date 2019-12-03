efaff40ce46ee729d1af97364ae3ef0134a39cb5
Qiyou Pang <qpang@ford.com>
2019-12-03 15:50:25 +0800

    init commit
    init commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+
+platform :ios, '10.0'
+
+target "SwiftLearn" do
+
+pod "RxSwift"
+pod "RxCocoa"
+pod "RxTest"
+pod "SnapKit"
+pod "SwiftyJSON"
+
+end
---
+++ SwiftLearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLearn
+//
+//  Created by mgc000005751 on 12/3/19.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
